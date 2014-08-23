eab6e3e59c2ba5ea20622f82a85d7bedd107c352
githubnorm <email@czepa.net>
2014-08-24 01:41:09 +0200

    php testing

---
+++ php/source/db_config.php
@@ -0,0 +1,18 @@
+<?php
+
+	$db = "test";
+	//$db = "live";
+		
+	/*test system*/
+	$host['test'] = "localhost";
+	$name['test'] = "root";
+	$pw['test'] = "";
+	$dbname['test'] = "jobsearch_test";
+	
+	/*live system*/
+	$host['live'] = "db540710562.db.1and1.com";
+	$name['live'] = "dbo540710562";
+	$pw['live'] = "SQLduke2982";
+	$dbname['live'] = "db540710562";
+	 
+?>
